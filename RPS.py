3a09d3c7ebb16c888a09dafdb22f1273ee8e4d3c
alex <bondareko.alex.sergeevich@gmail.com>
2025-10-07 01:33:04 +0200

    Make player 1 win more than 60% in each series

--- RPS.py
+++ RPS.py
@@ -1,10 +1,18 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
+import random
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
+    guess = prev_play
+    if guess == '':
+        guess = "R"
+    response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    last_three = set(opponent_history[-3:])
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(last_three) == 3:
+        response = {'S': 'P', 'P': 'R', 'R': 'P'}
+    elif len(last_three) == 2:
+        response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    elif len(last_three) == 1:
+        response = {'P': 'P', 'R': 'R', 'S': 'S'}
 
-    return guess
+
+    return response[guess]
